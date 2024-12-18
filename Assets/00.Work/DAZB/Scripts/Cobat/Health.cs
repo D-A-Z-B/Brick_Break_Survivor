9396f4a3666243d9f62b05a250d653b71b10bba8
ujn007 <hyunjun9531@gmail.com>
2024-12-19 01:48:21 +0900

    Die

--- Assets/00.Work/DAZB/Scripts/Cobat/Health.cs
+++ Assets/00.Work/DAZB/Scripts/Cobat/Health.cs
@@ -3,8 +3,10 @@ using BBS.Entities;
 using BBS.Players;
 using UnityEngine;
 
-namespace BBS.Combat {
-    public abstract class Health : MonoBehaviour, IEntityComponent, IDamageable {
+namespace BBS.Combat
+{
+    public abstract class Health : MonoBehaviour, IEntityComponent, IDamageable
+    {
         [SerializeField] protected int maxHealth;
         [SerializeField] protected int currentHealth;
 
@@ -13,36 +15,45 @@ namespace BBS.Combat {
 
         protected Player player;
 
-        public virtual int CurrentHealth {
-            get=> currentHealth;
-            set {
+        public virtual int CurrentHealth
+        {
+            get => currentHealth;
+            set
+            {
                 currentHealth = value;
 
-                if (currentHealth < 0) {
+                if (currentHealth < 0)
+                {
                     currentHealth = 0;
                 }
-                else if (currentHealth > maxHealth) {
+                else if (currentHealth > maxHealth)
+                {
                     currentHealth = maxHealth;
                 }
             }
         }
+
+        public int MaxHealth => maxHealth;
 
         public void Initialize(Entity entity)
         {
             player = entity as Player;
         }
 
-        private void Start() {
+        private void Start()
+        {
             currentHealth = maxHealth;
         }
 
-        public virtual void ApplyDamage(ActionData data) { 
+        public virtual void ApplyDamage(ActionData data)
+        {
             currentHealth -= data.damage;
             Debug.Log("appply damage");
 
             OnHit?.Invoke();
 
-            if (currentHealth <= 0 && player.IsDead == false) {
+            if (currentHealth <= 0 && player.IsDead == false)
+            {
                 Debug.Log("주금");
                 OnDead?.Invoke();
             }
