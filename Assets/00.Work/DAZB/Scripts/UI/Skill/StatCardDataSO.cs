d7ea1675a432c3adec8b08b047607742c3aed252
DAZB <jiwoo2426p@gmail.com>
2024-12-19 14:17:07 +0900

    skill

---
+++ Assets/00.Work/DAZB/Scripts/UI/Skill/StatCardDataSO.cs
@@ -0,0 +1,23 @@
+using BBS.Combat;
+using BBS.Players;
+using UnityEngine;
+
+namespace BBS.UI.Skills {
+    [CreateAssetMenu(fileName = "StatCardDataSO", menuName = "SO/StatCardData")]
+    public class StatCardDataSO : ScriptableObject {
+        public StatCardType type;
+        public string displayName;
+        public string description;
+        public Sprite icon;
+        public int increaseHp;
+        public int increaseDR;
+        public int healHp;
+
+        public void ApplyEffect() {
+            Player player = PlayerManager.Instance.Player;
+            player.GetCompo<Health>(true).CurrentHealth += (int)(player.GetCompo<Health>(true).MaxHealth * ((float)healHp / 100));
+            player.GetCompo<Health>(true).MaxHealth += increaseHp;
+            player.GetCompo<Health>(true).Dr += increaseDR;
+        }
+    }
+}
