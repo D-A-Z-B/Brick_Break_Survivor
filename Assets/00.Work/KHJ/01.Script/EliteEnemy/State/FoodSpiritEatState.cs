f71762bbe17a50992ef622c3f0e74caf16552ed5
ujn007 <hyunjun9531@gmail.com>
2024-12-20 00:47:09 +0900

    .

--- Assets/00.Work/KHJ/01.Script/EliteEnemy/State/FoodSpiritEatState.cs
+++ Assets/00.Work/KHJ/01.Script/EliteEnemy/State/FoodSpiritEatState.cs
@@ -7,8 +7,16 @@ namespace BBS.Enemies
 {
     public class FoodSpiritEatState : EntityState
     {
+        Enemy enemy;
         public FoodSpiritEatState(Entity entity, AnimParamSO stateAnimParam) : base(entity, stateAnimParam)
         {
+            enemy = entity as Enemy;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            enemy.ChangeState("IDLE");
         }
     }
 }
